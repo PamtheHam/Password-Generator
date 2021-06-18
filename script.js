366a5ecf4f7a767abb7e14bfc53e6eb300f14b3d
Pamela Hammond <hammondpamelac@gmail.com>
2021-06-18 16:13:11 -0400

    added functions for each character type, linked html to local js and css.

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+console.log(getRandomUpper());
+
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+console.log(getRandomLower());
+
+
+function getRandomSpecial() {
+  const special = '!@#$%^&*<>?';
+  return special[Math.floor(Math.random() * special.length)];
+}
+console.log(getRandomSpecial());
+
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+console.log(getRandomNumber());
